0669939b75d2f4a7152930a4a20768e16cbac05b
MShkarbanenko <mashkarbanenko@gmail.com>
2025-06-07 14:40:47 +0300

    feat: add 4_median_of_two_sorted_arrays.py

--- 9_palindrome_number.py
+++ 9_palindrome_number.py
@@ -1,1 +1,10 @@
-print([] == [])
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        revx = 0
+        d = x
+        while d != 0:
+            revx = revx * 10 + d % 10
+            d //= 10
+        return revx == x
