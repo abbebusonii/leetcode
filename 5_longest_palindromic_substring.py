681ee3c13fb2357ec66806b03ef5233e95842f43
MShkarbanenko <mashkarbanenko@gmail.com>
2025-06-08 10:37:14 +0300

    perf: update 5_longest_palindromic_substring.py
    - Time complexity: O(n^2)
    - Space complexity: O(n^2)

--- 5_longest_palindromic_substring.py
+++ 5_longest_palindromic_substring.py
@@ -1,14 +1,17 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def check(i, j):
-            while i < j:
-                if s[i] != s[j]:
-                    return False
-                i += 1
-                j -= 1
-            return True
         n = len(s)
-        for l in range(n, 0, -1):
-            for start in range(n - l + 1):
-                if check(start, start + l - 1):
-                    return s[start:start + l]
+        matrix = [[False for _ in range(n)] for _ in range(n)]
+        ans = [0, 0]
+        for l in range(1, n + 1):
+            for i in range(n - l + 1):
+                j = i + l - 1
+                if l == 1:
+                    matrix[i][j] = True
+                elif l == 2:
+                    matrix[i][j] = s[i] == s[j]
+                else:
+                    matrix[i][j] = s[i] == s[j] and matrix[i + 1][j - 1]
+                if matrix[i][j] and l > ans[1] - ans[0] + 1:
+                    ans = [i, j]
+        return s[ans[0]:ans[1] + 1]
