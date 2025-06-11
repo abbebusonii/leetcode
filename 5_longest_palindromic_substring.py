9975283067b3258cb1975e520c6ceb7396a1632a
MShkarbanenko <mashkarbanenko@gmail.com>
2025-06-11 13:43:59 +0300

    perf: add 5_longest_palindromic_substring.py
    - Manacher's algorithm
    - Time complexity: O(n)
    - Space complexity: O(n)

--- 5_longest_palindromic_substring.py
+++ 5_longest_palindromic_substring.py
@@ -1,23 +1,21 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        def check(i, j):
-            l = 0
-            while i >= 0 and j < n:
-                if s[i] != s[j]:
-                    break
-                l = j - i + 1
-                i -= 1
-                j += 1
-            return l
-        ans = [0, 0]
-        for i in range(1, n):
-            l_odd = check(i, i)
-            l_even = check(i - 1, i)
-            if l_odd > ans[1] - ans[0] + 1:
-                half = (l_odd - 1) // 2
-                ans = [i - half, i + half]
-            if l_even > ans[1] - ans[0] + 1:
-                half = l_even // 2
-                ans = [i - half, i + half - 1]
-        return s[ans[0]:ans[1] + 1]
+        s_hash = "#" + "#".join(c for c in s) + "#"
+        n = len(s_hash)
+        lp = [0 if i % 2 == 0 else 1 for i in range(n)]
+        center = bound = ans = 0
+        for i in range(n):
+            if i > bound:
+                center = bound = i
+            else:
+                lp[i] = min(lp[2 * center - i], bound - i)
+            r = lp[i]
+            while i - r - 1 >= 0 and i + r + 1 < n and s_hash[i - r - 1] == s_hash[i + r + 1]:
+                r += 1
+                lp[i] += 2 * (s_hash[i + r] != "#")
+            if i + r > bound:
+                center = i
+                bound = i + r
+            if lp[i] > lp[ans]:
+                ans = i
+        return s[(ans - lp[ans]) // 2:(ans + lp[ans]) // 2]
