d47b0e367223f435bff7763c90bd53a7cd5025da
MShkarbanenko <mashkarbanenko@gmail.com>
2025-06-13 11:24:15 +0300

    refactor: p7_reverse_integer.py
    - Digits manipulation approach

--- src/p7_reverse_integer.py
+++ src/p7_reverse_integer.py
@@ -1,16 +1,10 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        sign = 1 if x >= 0 else -1
-        bound = "2147483647" if sign == 1 else "2147483648"
-        x = str(x)[::-1] if sign == 1 else str(x)[1:][::-1]
-        n = len(x)
-        if n < len(bound):
-            return sign * int(x)
-        i = 0
-        while i < n:
-            if x[i] > bound[i]:
+        sign = [1, -1][x < 0]
+        rx, x = 0, abs(x)
+        while x:
+            x, mod = divmod(x, 10)
+            rx = rx * 10 + mod
+            if rx > 2**31 - 1:
                 return 0
-            elif x[i] < bound[i]:
-                break
-            i += 1
-        return sign * int(x)
+        return sign * rx
