f4e020385abd5308e2e60966a8d19080ec1dee11
MShkarbanenko <mashkarbanenko@gmail.com>
2025-06-13 11:57:07 +0300

    style: update p7_reverse_integer.py
    - Change variable names to more appropriate ones

--- src/p7_reverse_integer.py
+++ src/p7_reverse_integer.py
@@ -1,10 +1,11 @@
 class Solution:
     def reverse(self, x: int) -> int:
         sign = [1, -1][x < 0]
+        bound = 2 ** 31 - 1
         rx, x = 0, abs(x)
         while x:
-            x, mod = divmod(x, 10)
-            rx = rx * 10 + mod
-            if rx > 2**31 - 1:
+            x, reminder = divmod(x, 10)
+            rx = rx * 10 + reminder
+            if rx > bound:
                 return 0
         return sign * rx
