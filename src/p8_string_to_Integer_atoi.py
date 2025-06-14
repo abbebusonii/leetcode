f4e74621f97187d68aa952d42e79719ec6a54670
MShkarbanenko <mashkarbanenko@gmail.com>
2025-06-14 11:01:44 +0300

    refactor: update p8_string_to_Integer_atoi.py
    - Make solution cleaner

--- src/p8_string_to_Integer_atoi.py
+++ src/p8_string_to_Integer_atoi.py
@@ -1,23 +1,20 @@
-class Solution:
-    def myAtoi(self, s: str) -> int:
-        ans = []
-        digits = set(str(i) for i in range(10))
-        s = s.lstrip(" ")
-        if not s:
-            return 0
-        sign = -1 if s[0] == "-" else 1
-        if s[0] in ("-", "+"):
-            s = s[1:]
-        if not s:
-            return 0
-        s = s.lstrip("0")
-        if not s:
-            return 0
-        for c in s:
-            if c not in digits:
-                break
-            ans.append(c)
-        if not ans:
-            return 0
-        ans = int("".join(c for c in ans))
-        return min(ans, 2 ** 31 - 1) if sign == 1 else max(sign * ans, -2 ** 31)
+class Solution(object):
+    def myAtoi(self, s):
+        i = 0
+        n = len(s)
+        while i < n and s[i] == " ":
+            i += 1
+        sign = 1
+        if i < n and (s[i] == "-" or s[i] == "+"):
+            sign = -1 if s[i] == "-" else 1
+            i += 1
+        ans = 0
+        lower_bound, upper_bound = -2 ** 31, 2 ** 31 - 1
+        while i < n and s[i].isdigit():
+            ans = ans * 10 + int(s[i])
+            if ans * sign > upper_bound:
+                return upper_bound
+            if ans * sign < lower_bound:
+                return lower_bound
+            i += 1
+        return ans * sign
