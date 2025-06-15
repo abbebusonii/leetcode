6af15284f38e3aeb6d9998610e645c5414f53676
MShkarbanenko <mashkarbanenko@gmail.com>
2025-06-15 08:34:00 +0300

    refactor: update p9_palindrome_number.py
    - Resolve it one more time

--- src/p9_palindrome_number.py
+++ src/p9_palindrome_number.py
@@ -2,9 +2,9 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
-        revx = 0
-        d = x
-        while d != 0:
-            revx = revx * 10 + d % 10
-            d //= 10
-        return revx == x
+        rx = 0
+        copy = x
+        while copy:
+            copy, reminder = divmod(copy, 10)
+            rx = 10 * rx + reminder
+        return rx == x
